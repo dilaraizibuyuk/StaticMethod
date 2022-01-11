97ed8180d5fed77398942a5ec85bf7ba0566dacd
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-11 16:00:58 +0300

    Proje dosyası ekle.

---
+++ StaticMethod/Personel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    class Personel  //static olmama durumu
+    {
+        //public void AdSoyadYazdir()
+        //{
+        //    Console.WriteLine("Stephen Hawking");
+        //}
+
+
+        //static olma durumu
+
+        //public static void AdSoyadYazdir()
+        //{
+        //    Console.WriteLine("Stephen Hawking");
+        //}
+
+        public static void numbersAddup(int number1,int number2)
+        {
+            Console.WriteLine("İki sayının toplamı:"+(number1+number2));
+        }
+    }
+}
---
+++ StaticMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Personel p1 = new Personel();
+            //p1.AdSoyadYazdir();
+
+
+            //static durum
+            //Personel.AdSoyadYazdir();
+
+
+            Personel.numbersAddup(58,8);
+            Console.ReadKey();
+        }
+    }
+}
